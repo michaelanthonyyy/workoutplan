a2a83540337ebd8198c2fca5adcd9177986473cc
Michael Medina <michaelanthony.medinaa@gmail.com>
2021-02-19 15:28:53 -0800

    added comments for routes needed to be created

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const path = require("path");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,6 +14,28 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbExample", { useNewUrlParser: true });
+
+
+// Route for exercise html app.get
+
+
+// Route for stats html app.get
+
+
+// route for workout data app.get 
+
+
+// update workout data app.put
+
+
+// populate page with workout data app.put
+
+
+// create new workout app.post
+
+
+// populate with range ? app.get
+
 
 
 app.listen(PORT, () => {
